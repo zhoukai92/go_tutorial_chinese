8e86ea8035ee96dbb3d0b9788a64d3d0b465b5c3
周凯 <kai.zhou3@amh-group.com>
2025-04-06 01:15:46 +0800

    修改文件夹名，edit README.md

--- dir00/a0.go
+++ dir00/a0.go
@@ -33,7 +33,10 @@ func demo01() {
 	)
 	//方式2：Scanf
 	fmt.Println("请录入学生的年龄，姓名，成绩，是否是VIP，使用空格进行分隔")
-	fmt.Scanf("%d %s %f %t", &age, &name, &score, &isVIP)
+	n, err := fmt.Scanf("%d %s %f %t", &age, &name, &score, &isVIP)
+	if err != nil {
+		fmt.Println(n, err)
+	}
 	//将上述数据在控制台打印输出：
 	fmt.Printf("学生的年龄为：%v,姓名为：%v,成绩为：%v,是否为VIP:%v", age, name, score, isVIP)
 }
